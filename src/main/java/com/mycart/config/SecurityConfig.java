bae20acdc0ac132dba4dff55bb609d74db89b62e
shiv-hub <shivkumarmunda0097@gmail.com>
2020-12-14 12:06:01 +0530

    allowed all request to resources without checking token for development after resolving CORS i will reinstate security

--- src/main/java/com/mycart/config/SecurityConfig.java
+++ src/main/java/com/mycart/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/ur/authenticate","/pvc/products")
+        http.csrf().disable().authorizeRequests().antMatchers("/**")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
