76b8559f932dcf4f69ab97581dec163ff077e0da
shiv-hub <shivkumarmunda0097@gmail.com>
2020-12-15 10:09:50 +0530

    Given proper names to users and added USER_LOGIN_DETAILS on db folder for reference during development

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,22 +1,22 @@
 INSERT INTO public.users
 (a_email, addr_id, created_ts, email, first_name, is_active, last_name, manager_id, modified_by_id, modified_ts, phone1, phone2, pwd, role_id, store_id, tag, user_name)
-VALUES('atestemail.com', 1, now(), 'test@email.com', 'sam', 'Y', 'simon', 1, 1, now(), '1234567890', '1234567890', 'abc1', 1, 1, 'something', 'test@email.com');
+VALUES('shivmunda1@gmail.com', 1, now(), 'shivmunda@gmail.com', 'Shiv', 'Y', 'Munda', 1, 1, now(), '87091194XX', '9785885XXX', 'shiv1', 1, 1, 'customer', 'shivmunda@gmail.com');
 
 INSERT INTO public.users
 (a_email, addr_id, created_ts, email, first_name, is_active, last_name, manager_id, modified_by_id, modified_ts, phone1, phone2, pwd, role_id, store_id, tag, user_name)
-VALUES('atestemail.com', 1, now(), 'test@email.com', 'sam', 'Y', 'simon', 1, 1, now(), '1234567890', '1234567890', 'abc1', 2, 1, 'something', 'test1@email.com');
+VALUES('madhugt1@gmail.com', 2, now(), 'madhugt@gmail.com', 'Madhusudan', 'Y', 'GT', 1, 1, now(), '99454449XX', '7884548XXX', 'madhu1', 2, 1, 'supplier', 'madhugt@email.com');
 
 INSERT INTO public.users
 (a_email, addr_id, created_ts, email, first_name, is_active, last_name, manager_id, modified_by_id, modified_ts, phone1, phone2, pwd, role_id, store_id, tag, user_name)
-VALUES('atestemail.com', 1, now(), 'test@email.com', 'sam', 'Y', 'simon', 1, 1, now(), '1234567890', '1234567890', 'abc1', 3, 1, 'something', 'test2@email.com');
+VALUES('swagatamandal1@gmail.com', 3, now(), 'swagatamandal@gmail.com', 'Swagata', 'Y', 'Mandal', 1, 1, now(), '84694981XX', '8004944XXX', 'swagata1', 3, 1, 'store owner', 'swagatamandal@gmail.com');
 
 INSERT INTO public.users
 (a_email, addr_id, created_ts, email, first_name, is_active, last_name, manager_id, modified_by_id, modified_ts, phone1, phone2, pwd, role_id, store_id, tag, user_name)
-VALUES('atestemail.com', 1, now(), 'test@email.com', 'sam', 'Y', 'simon', 1, 1, now(), '1234567890', '1234567890', 'abc1', 4, 1, 'something', 'test3@email.com');
+VALUES('umeshkatakam1@gmail.com', 4, now(), 'umeshkatam@gmail.com', 'Umesh', 'Y', 'Katakam', 1, 1, now(), '78784499XX', '98711545XXX', 'umesh1', 4, 1, 'store manager', 'umeshkatakam@gmail.com');
 
 INSERT INTO public.users
 (a_email, addr_id, created_ts, email, first_name, is_active, last_name, manager_id, modified_by_id, modified_ts, phone1, phone2, pwd, role_id, store_id, tag, user_name)
-VALUES('atestemail.com', 1, now(), 'test@email.com', 'sam', 'Y', 'simon', 1, 1, now(), '1234567890', '1234567890', 'abc1', 5, 1, 'something', 'test4@email.com');
+VALUES('viswachandreddy1@gmail.com', 5, now(), 'viswachandreddy@gmail.com', 'Viswachand', 'Y', 'Reddy', 1, 1, now(), '98771499XX', '8994944XXX', 'viswachand1', 5, 1, 'super user', 'test4@email.com');
 
 INSERT INTO public.product_view
 (brand, category_id, created_ts, description, discount, end_date, exp_date, mfd_date, modified_ts, "name", price, start_date)
